58dcae0671ddf8148266beaa1d9ee87befb768ce
rathaumons <ratha.siv@student.umons.ac.be>
2022-03-22 16:05:57 +0100

    Update info

--- make.bat
+++ make.bat
@@ -1,17 +1,18 @@
 @ECHO OFF
 
-REM Command file build and install DCNv2
+REM Command file: Build and install DCNv2
 
 ECHO. 
-ECHO ========================================================
+ECHO =========================================================
 ECHO Important:
-ECHO --------------------------------------------------------
+ECHO ---------------------------------------------------------
 ECHO Make sure you already added the correct path of 'cl.exe'
 ECHO of VS2019 in system path variable before running this.
-ECHO ========================================================
+ECHO =========================================================
 ECHO. 
 
-ECHO DCNv2 will be built an installed after you press any key
+ECHO DCNv2 will be built and installed after you press any key
+ECHO.
 
 PAUSE
 
